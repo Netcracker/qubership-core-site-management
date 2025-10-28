c15dd977d23749e1f0b9bf65bc8669180be4c34f
Vladislav Larkin <larkin.qubership@gmail.com>
2025-10-28 20:12:44 +0400

    feat: remove 'qubership.org' api group (#56)
    * feat: remove 'qubership.org' api group
    
    * chore: remove extra logic with NewResourceGroupAnnotationsMapper

--- site-management-service/server.go
+++ site-management-service/server.go
@@ -4,7 +4,6 @@ import (
 	fiberSec "github.com/netcracker/qubership-core-lib-go-fiber-server-utils/v2/security"
 	"github.com/netcracker/qubership-core-lib-go/v3/security"
 	"github.com/netcracker/qubership-core-lib-go/v3/serviceloader"
-	"github.com/netcracker/qubership-core-lib-go/v3/utils"
 	"github.com/netcracker/qubership-core-site-management/site-management-service/v2/idp"
 	"github.com/netcracker/qubership-core-site-management/site-management-service/v2/lib"
 )
@@ -12,7 +11,6 @@ import (
 func init() {
 	serviceloader.Register(1, &idp.DummyRetryableClient{})
 	serviceloader.Register(1, &security.DummyToken{})
-	serviceloader.Register(1, utils.NewResourceGroupAnnotationsMapper("qubership.cloud"))
 	serviceloader.Register(1, &fiberSec.DummyFiberServerSecurityMiddleware{})
 }
 
